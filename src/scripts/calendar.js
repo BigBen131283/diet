e0a51660260e2ebb2035cbcf14563290223908da
BigBen83 <benjamin.toubhans@orange.fr>
2023-07-09 23:06:55 +0200

    230709 - generating tabs part1

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -1,3 +1,5 @@
+import Tab from "./tab.js";
+
 let date = new Date();
 
 const addCalendarBtn = document.querySelector('.add-month');
@@ -6,8 +8,8 @@ const calendar = document.querySelector('.calendar')
 
 const months =  [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
 const mois = [ 'Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aout', 'Sep', 'Oct', 'Nov', 'Dec' ];
-const joursSemaine = ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'];
-const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'SaT', 'Sun'];
+const joursSemaine = ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'];
+const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'SaT'];
 
 function getCurrentDate() {
     let day = date.getDate();
@@ -23,35 +25,86 @@ function getCurrentDate() {
     }
 }
 
-let currentDate = getCurrentDate();
-console.log(currentDate);
+function getFirstDayofMonth(year, month) {
+    const date = new Date(year, month, 1);
+    const options = { weekday: 'long'};
+    const dayName = date.toLocaleString('fr-FR', options);
+
+    return dayName;
+}
+
+console.log(getFirstDayofMonth(2023, 11));
+
 
 /////////////////////////////////////////////////////////////////////////////////
 // Gestion des onglets
 /////////////////////////////////////////////////////////////////////////////////
 
-const tabMonths = Array.from(document.querySelectorAll('.month'));
+let tabMonths = [];
 const monthContents = Array.from(document.querySelectorAll('.days-container'));
 
-tabMonths.forEach(tab => {
-    tab.addEventListener('click', (e) => {
-        
-        for(let i=0; i < tabMonths.length; i++){
-            if(tabMonths[i] === e.target){
-                tabMonths[i].classList.add('is-active');
-            }else{
-                tabMonths[i].classList.remove('is-active');
+function handleTab() {
+    
+    tabMonths = Array.from(document.querySelectorAll('.month'));
+    tabMonths.forEach(tab => {
+        tab.addEventListener('click', (e) => {
+            
+            for(let i=0; i < tabMonths.length; i++){
+                if(tabMonths[i] === e.target){
+                    tabMonths[i].classList.add('is-active');
+                }else{
+                    tabMonths[i].classList.remove('is-active');
+                }
             }
-        }
-        
-        for(let i=0; i < monthContents.length; i++){
-            if(i === tabMonths.indexOf(e.target)){
-                monthContents[i].classList.add('is-active');
-            }else{
-                monthContents[i].classList.remove('is-active');
+            
+            for(let i=0; i < monthContents.length; i++){
+                if(i === tabMonths.indexOf(e.target)){
+                    monthContents[i].classList.add('is-active');
+                }else{
+                    monthContents[i].classList.remove('is-active');
+                }
             }
-        }
+        })
     })
-})
+
+}
+
 /////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////////////////////////////////////////////////////////
+// Ajout d'un mois
+/////////////////////////////////////////////////////////////////////////////////
+
+const addMonthBtn = document.querySelector('.add-tab');
+const tabs = document.querySelector('.tab-content');
+
+addMonthBtn.addEventListener('click', () => {
+    addTabMonth();
+    handleTab();
+})
+
+function addTabMonth() {
+    
+    console.log(tabMonths.length);
+
+    const arrayTabs = [];
+    
+    if(tabMonths.length === 0){
+        let currentDate = getCurrentDate();
+        const newTab = document.createElement('div');
+        newTab.classList.add('month');
+        newTab.innerText = currentDate.monthName;
+        tabs.appendChild(newTab);
+        tabMonths.push(newTab);
+
+        const newTabObj = new Tab();
+        arrayTabs.push(newTabObj);
+        console.log(arrayTabs);
+
+        // console.log(currentDate.monthName);
+    }else{
+        console.log(tabMonths);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////
---
+++ src/scripts/tab.js
@@ -0,0 +1,8 @@
+export default class Tab {
+    constructor (firstDay, month, year) {
+        this.firstDay = firstDay;
+        this.month = month;
+        this.year = year;
+    }
+
+}
